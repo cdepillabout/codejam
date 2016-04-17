d0b83ff2e2a2c0ecec415fa13abdccc6c2f55e06
(cdep)illabout <cdep.illabout@gmail.com>
2016-04-17 14:30:50 +0900

    Updating round 1 b file.

--- src/Round1ABRankAndFile.hs
+++ src/Round1ABRankAndFile.hs
@@ -1,24 +1,68 @@
+{-# LANGUAGE LambdaCase #-}
 
 module Round1ABRankAndFile where
 
-import ClassyPrelude hiding (Vector)
+import ClassyPrelude hiding (Index, Vector)
 
 import Data.Int
+import Data.Array
 
 type RowOrColumn = [Int]
 type SideLength = Int
+type Row = Int
+type Column = Int
+type CurrentRow = Int
+type CurrentColumn = Int
+type Index = (Row, Column)
+
+type Field = (SideLength, CurrentRow, CurrentColumn, Array (Int, Int) (Maybe Int))
 
 sortInput :: [RowOrColumn] -> [RowOrColumn]
 sortInput = sort
 
+addFirstRow :: Field -> RowOrColumn -> Field
+addFirstRow (sideLength, 0, 0, field) row =
+    (sideLength, 1, 0, field // fmap f (zip [0..] row))
+  where
+    f :: (Column, Int) -> (Index, Maybe Int)
+    f (col, item) = ((0, col), Just item)
+addFirstRow _ _ = error "got an error"
+
+tryAddRow :: Field -> RowOrColumn -> Maybe Field
+tryAddRow (sideLength, curRow, curCol, field) row =
+    undefined
+
+addToField :: Field -> RowOrColumn -> Field
+addToField field rowOrColumn = undefined
+
 solve :: SideLength -> [RowOrColumn] -> RowOrColumn
 solve sideLength rowOrColumnList = undefined
 
+emptyField :: SideLength -> Field
+emptyField sideLength =
+    (sideLength, 0, 0,
+        array ((0, 0), (len, len))
+            [ ((x, y), Nothing) | x <- [0..len], y <- [0..len] ])
+  where
+    len = sideLength - 1
+
+printField :: Field -> IO ()
+printField (_, _, _, field) =
+    let items = assocs field
+        sameRowItems = groupBy (\((aX, _), _) ((bX, _), _) -> aX == bX) items
+    in for_ sameRowItems $ \sameRow -> do
+        for_ sameRow $ \case
+                            ((_, _), Just num) -> putStr $ tshow num <> " "
+                            ((_, _), Nothing) -> putStr $ "_" <> " "
+        putStrLn ""
+
 exampleRows :: [RowOrColumn]
 exampleRows =
-    [ [ 1, 2, 8 ]
-    , [ 2, 5, 8 ]
-    , [ 1, 3, 7 ]
+    [ [ 1, 2, 3 ]
+    , [ 2, 3, 5 ]
+    , [ 3, 5, 6 ]
+    , [ 2, 3, 4 ]
+    , [ 1, 2, 3 ]
     ]
 
 lastWordDefaultMain :: IO ()
